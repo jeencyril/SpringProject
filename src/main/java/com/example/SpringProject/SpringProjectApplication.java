46e34be7e408ab9bff40fca4e643cca32923d00e
jeencyril <jeencyril@yahoo.com>
2023-08-25 17:25:40 -0600

    code

---
+++ src/main/java/com/example/SpringProject/SpringProjectApplication.java
@@ -0,0 +1,21 @@
+package com.example.SpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+/*
+echo "# SpringProject" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jeencyril/SpringProject.git
+git push -u origin main
+ */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
